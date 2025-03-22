adc87bbfb3ea220a56af7c519b8ef90a2ad668a4
Dex <dexter@humanlayer.dev>
2025-03-22 16:50:20 -0500

    Update hack/git_pre_push.sh

--- hack/git_pre_push.sh
+++ hack/git_pre_push.sh
@@ -2,7 +2,7 @@ CHANGED_FILES=$(git diff --name-only upstream/main)
 
 if echo "${CHANGED_FILES}" | grep -qE '^kubechain/'; then
 	echo ": -- 🚀 kubechain --"
-	make -C kubechain test
+	make -C kubechain test lint
 else
 	echo ": -- ⏭️ kubechain --"
 fi
